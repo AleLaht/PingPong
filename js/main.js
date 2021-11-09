dc1132fb38bc5683f84ab41a48c733453fcb3de5
AleLaht <lahtinen.ale@gmail.com>
2021-11-09 16:15:45 +0200

    created draw functions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d')
 
+let paddleHeight = 50;
+let paddleWidth = 10;
+
 const ai = {
-    width: 10,
-    height: 50,
-    x: 10,
-    y: canvas.height / 2
+    width: paddleWidth,
+    height: paddleHeight,
+    x: canvas.width - paddleWidth,
+    y: canvas.height / 2 - paddleHeight / 2
 }
 
 const player = {
-    width: 10,
-    height: 50,
+    width: paddleWidth,
+    height: paddleHeight,
     x: 0,
-    y: canvas.height / 2
+    y: canvas.height / 2 - paddleHeight / 2
 }
 
 const ball = {
@@ -23,3 +26,34 @@ const ball = {
     x: canvas.width / 2,
     y: canvas.height / 2
 }
+
+const drawPlayer = () => {
+    ctx.beginPath();
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+const drawBall = () => {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ball.x = ball.x + ball.velocityX;
+    ball.y = ball.y + ball.velocityY;
+    console.log('asd')
+}
+
+const drawAi = () => {
+    ctx.beginPath();
+    ctx.fillRect(ai.x, ai.y, ai.width, ai.height);
+}
+
+const render = () => {
+    drawBall();
+    drawPlayer();
+    drawAi();
+}
+
+const play = () => {
+    render()
+}
+
+setInterval(play, 100);
